083f466d18efc252eb228b32723480f0cebc744c
Olga Rosas <olga.rosas@gmail.com>
2019-06-18 23:18:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each do |i|
     new_array << i = i ** 2
+=======
+  new array = []
+  array.push = array.each do |i|
+    i = i ** 2
+>>>>>>> 7ff93ee21b63089ef588520e983c223c9d795d4f
   end
   return new_array
 end
