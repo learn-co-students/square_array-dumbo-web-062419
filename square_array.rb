46bf4d3fb414fbb8c88156673c0457f51eacfc35
Olga Rosas <olga.rosas@gmail.com>
2019-06-18 23:19:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,4 @@
+=begin
 def square_array(array)
 <<<<<<< HEAD
   new_array = []
@@ -10,4 +11,10 @@ def square_array(array)
 >>>>>>> 7ff93ee21b63089ef588520e983c223c9d795d4f
   end
   return new_array
+end
+=end
+
+def square_array(array)
+array.collect { |i| i ** 2 }
+return array 
 end
